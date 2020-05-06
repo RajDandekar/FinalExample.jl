45ab653d76fe530690c1d2094c60fef9eac9cb79
Raj <rajd@mit.edu>
2020-05-06 12:38:59 -0400

    Run tests

--- src/FinalExample.jl
+++ src/FinalExample.jl
@@ -2,4 +2,8 @@ module FinalExample
 
 greet() = print("Hello World!")
 
+include("extrafile_final.jl")
+
+export my_fu
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
+Pkg.activate(".")
+
 using FinalExample
 using Test
 
+
 @testset "FinalExample.jl" begin
+    @test my_fu(2, 1) == 7
+    @test my_fu(2, 3) == 13
     # Write your own tests here.
 end
